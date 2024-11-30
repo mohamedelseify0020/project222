345cfd1a19e08b6f840b0f1c8b52c83894f4dd09
mohamedelseify <mohamedelseify01@gmail.com>
2024-11-30 11:39:16 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/*document.getElementById('contactForm').addEventListener('submit', function(event) {  
+    event.preventDefault(); // منع إرسال النموذج  
+    const name = document.getElementById('name').value;  
+    const email = document.getElementById('email').value;  
+    alert('شكراً لتواصلك، ' + name + '! سنتواصل معك عبر البريد الإلكتروني: ' + email);  
+});*/  
